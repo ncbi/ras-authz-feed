dfdc246b7963414167d35c1d393cbe4487c1f73c
kdurbrow <durbrowk@ncbi.nlm.nih.gov>
2021-03-09 11:19:52 -0500

    Initial commit

---
+++ schema/VDS_access.sql
@@ -0,0 +1,26 @@
+CREATE TABLE VDS_access
+(
+	[user_id] varchar (50) NOT NULL,
+	_pid int NOT NULL,
+	authority varchar (50) NOT NULL,
+	study varchar (50) NOT NULL,
+	consent int NOT NULL,
+	[role] varchar (50) NOT NULL,
+	status varchar (50) NOT NULL,
+	_acid int NOT NULL,
+	expires datetime NULL,
+	updated datetime NOT NULL,
+	downloader_for varchar (50) NOT NULL DEFAULT (''),
+	created datetime NOT NULL DEFAULT (getdate()),
+	consent_name varchar (200) NULL,
+	consent_accession varchar (50) NULL,
+        CONSTRAINT PK_VDS_access PRIMARY KEY CLUSTERED 
+        (
+                [user_id],
+	        authority,
+	        study,
+	        consent,
+	        _acid,
+	        downloader_for
+        )
+)
